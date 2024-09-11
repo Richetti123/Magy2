57cc325d112163e1f67262c4ee0e680878575842
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-09-11 21:36:11 +0200

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,11 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `@comu.blessed\n@comu.blessed_fem ${pesan}`
-let teks = `*💛DESPIERTEN, HAY ACTIVIDAD🧡*\n${oi}\n\n*MENCIONES:*\n`
+let oi = `@comu.blessed_fem ${pesan}`
+let teks = `*🩷DESPIERTEN, HAY ACTIVIDAD🩵*\n${oi}\n\n*MENCIONES:*\n`
 for (let mem of participants) {
 teks += `🩷💛 @${mem.id.split('@')[0]}\n`}
-teks += `MagyBot`
+teks += `@comunidad.td12\n@comu.blessed`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
